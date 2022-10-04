1654df1861c5b5dd2490662fb08ef556260ca56f
Karol Galera <frontend.galera@gmail.com>
2022-10-04 16:12:56 +0200

    GlobalStyle added to app

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,11 +1,14 @@
 import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
+      <GlobalStyle />
+      <div>Hello world!</div>
     </ThemeProvider>
   )
 };
