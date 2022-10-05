8b19819fd15ecb9f0c1b0ff689cc1043128c8e91
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-05 10:30:18 +0200

    added Container

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,13 +2,16 @@ import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import { Container } from "./styled";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <div>Hello world!</div>
+      <Container>
+        
+      </Container>
     </ThemeProvider>
   )
 };
