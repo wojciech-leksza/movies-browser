dff4b9e8d1e80ce5be8a996672f98961327f80b9
Karol Galera <frontend.galera@gmail.com>
2022-10-05 17:35:10 +0200

    component Navigation import to app

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,13 +2,14 @@ import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import Navigation from "../../common/Navigation";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <div>Hello world!</div>
+      <Navigation />
     </ThemeProvider>
   )
 };
