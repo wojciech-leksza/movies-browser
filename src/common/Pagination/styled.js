3b66dfa2229d2fe61984cdb5cc69f84e9c007f26
Karol Galera <frontend.galera@gmail.com>
2022-10-06 17:01:30 +0200

    fonts taken from theme, box-shadow on hover added to Button and margin added to Wrapper

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -6,7 +6,7 @@ export const Wrapper = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 40px;
+  margin: 40px 0 100px 0;
 `;
 
 export const Button = styled.button`
@@ -18,22 +18,24 @@ export const Button = styled.button`
   color: ${({ theme }) => theme.colors.woodSmoke};
   border: none;
   margin: 0 12px;
+  transition: box-shadow 0.5s;
 
   &:hover {
     cursor: pointer;
+    box-shadow: ${({ theme }) => theme.boxShadow.hover};
   }
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     margin: 0 8px;
   }
 `;
 
 export const Text = styled.span`
   font-weight: 400;
-  font-size: 14px;
+  font-size: ${({ theme }) => theme.fontSizes.small};
   margin: 0 12px;
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     display: none;
   }
 `;
@@ -41,10 +43,10 @@ export const Text = styled.span`
 export const Paragraph = styled.p`
   font-weight: 400;
   margin: 0 12px;
-  font-size: 16px;
+  font-size: ${({ theme }) => theme.fontSizes.medium};
   color: ${({ theme }) => theme.colors.darkGrey};
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     margin: 0;
   }
 `;
@@ -52,10 +54,10 @@ export const Paragraph = styled.p`
 export const PageNumber = styled.span`
   font-weight: 600;
   margin: 0 8px;
-  font-size: 16px;
+  font-size: ${({ theme }) => theme.fontSizes.medium};
   color: ${({ theme }) => theme.colors.woodSmoke};
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     margin: 0 2px;
   }
 `;
@@ -73,7 +75,7 @@ export const RightArrow = styled(VectorRight)`
 export const LeftArrowMobile = styled(VectorLeft)`
   display: none;
   
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     display: inline;
     color: ${({ theme }) => theme.colors.blue};
     margin-right: 8px;
@@ -83,7 +85,7 @@ export const LeftArrowMobile = styled(VectorLeft)`
 export const RightArrowMobile = styled(VectorRight)`
   display: none;
   
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     display: inline;
     color: ${({ theme }) => theme.colors.blue};
     margin-left: 8px;
