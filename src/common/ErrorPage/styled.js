e2dc5596e11d19d49b60586fc2527e79d6df7068
Karol Galera <frontend.galera@gmail.com>
2022-10-06 17:54:38 +0200

    cosmetic style changes

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -11,85 +11,82 @@ export const Container = styled.main`
 export const StyledIco = styled(ErrorElement)`
     margin: 120px 0px 40px;
     width: 120px;
-    height:auto;
+    height: auto;
 
     @media (max-width:${({ theme }) => theme.breakpoints.tabletMax}px) {
         width: 80px;
-        height:auto;
         margin: 80px 0px 20px;
     }
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}px) {
         width: 40px;
-        height:auto;
         margin: 40px 0px 10px;
     }
 `
 
 export const Title = styled.h1`
     font-weight: 600;
-    font-size: 36px;
+    font-size: ${({ theme }) => theme.fontSizes.larger};
     line-height: 120%;
     margin: 0px;
 
     @media (max-width:${({ theme }) => theme.breakpoints.tabletMax}px) {
-        font-size: 21px;
+        font-size: ${({ theme }) => theme.fontSizes.large};
     }
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}px) {
         font-weight: 500;
-        font-size: 14px;
+        font-size: ${({ theme }) => theme.fontSizes.small};
         line-height: 130%;
     }
 `
 
 export const SubTitle = styled.h2`
     font-weight: 500;
-    font-size: 22px;
+    font-size: ${({ theme }) => theme.fontSizes.large};
     line-height: 130%;
     text-align: center;
     margin: 24px;
 
     @media (max-width:${({ theme }) => theme.breakpoints.tabletMax}px) {
-        font-size: 15px;
+        font-size: ${({ theme }) => theme.fontSizes.medium};
         margin: 15px;
     }
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}px) {
-        font-size: 9px;
+        font-size: ${({ theme }) => theme.fontSizes.mobileSmall};
         margin: 8px;
     }
 `
 
 export const Button = styled.button`
-    width: 180px;
-    height: 50px;
-    border-radius: 5px;
+    padding: 16px 24px;
+    border-radius: ${({theme}) => theme.borderRadiusSmall};
     color:${({ theme }) => theme.colors.white};
     background-color: ${({ theme }) => theme.colors.blue};
     font-weight: 700;
-    font-size: 14px;
-    line-height: 19px;
-    border:none;
-    cursor: pointer;
-    :hover{
+    font-size: ${({ theme }) => theme.fontSizes.small};
+    border: none;
+    transition: box-shadow 0.3s;
+    
+    &:hover{
         filter:brightness(110%);
+        cursor: pointer;
+        box-shadow: ${({ theme }) => theme.boxShadow.hover};
     }
-    :active{
+
+    &:active{
         filter:brightness(90%);
     }
 
     @media (max-width:${({ theme }) => theme.breakpoints.tabletMax}px) {
         border-radius: 4px;
-        font-size: 9px;
-        width: 120px;
-        height: 35px;
+        font-size: ${({ theme }) => theme.fontSizes.mobileMedium};
     }
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}px) {
         border-radius: 3px;
-        font-size: 5px;
-        width: 70px;
-        height: 20px;
+        font-size: ${({ theme }) => theme.fontSizes.mobileSmall};
+        padding: 8px 12px;
     }
 `
