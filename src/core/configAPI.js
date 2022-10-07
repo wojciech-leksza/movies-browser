07fcbdedce3fc0b4850d1272c1bd666dc5ee6dc9
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-07 19:32:52 +0200

    connection to api

---
+++ src/core/configAPI.js
@@ -0,0 +1,3 @@
+export const popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular";
+export const api_key = "yours api_key here";
+export const language = "en-US";
--- src/core/moviesAPI.js
+++ src/core/moviesAPI.js
@@ -1,9 +1,19 @@
-import examplePopularMovies from "./examplePopularMovies.json";
+import axios from "axios";
+import { api_key, language, popularMoviesUrl } from "./configAPI";
 
-const getPopularMovies = () => examplePopularMovies;
-
-const getMovies = () => {
-    return getPopularMovies();
+const getMovies = async (page = 1) => {
+    try {
+        const response = await axios.get(popularMoviesUrl, {
+            params: {
+                api_key,
+                language,
+                page,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(error);
+    };  
 };
 
 export default getMovies;
