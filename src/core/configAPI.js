0fb634d512683a41c12da45893da6f40c29efa16
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-08 20:53:40 +0200

    added searchMoviesUrl to configAPI.js

--- src/core/configAPI.js
+++ src/core/configAPI.js
@@ -1,3 +1,4 @@
 export const popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular";
+export const searchMoviesURL = "https://api.themoviedb.org/3/search/movie";
 export const api_key = "yours api_key here";
 export const language = "en-US";
