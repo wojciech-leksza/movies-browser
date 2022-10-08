e661c6ba23ab0d5e48c28b6c2d67a9f4ff88b832
Karol Galera <frontend.galera@gmail.com>
2022-10-08 14:35:54 +0200

    selector and dispatch added for action that fetch movies from API

--- src/features/MoviesPage/index.js
+++ src/features/MoviesPage/index.js
@@ -1,21 +1,27 @@
-import { Wrapper } from "./styled";
 import MovieCard from "./MovieCard";
 import Container from "../../common/Container";
 import Pagination from "../../common/Pagination";
 import Navigation from "../../common/Navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, selectMovies } from "./slice";
+import { useEffect } from "react";
+import getMovies from "../../core/moviesAPI";
 
 const MoviesPage = () => {
+  const movies = useSelector(selectMovies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMovies(getMovies));
+  }, [dispatch]);
+
   return (
     <>
       <Navigation />
       <Container title="Popular Movies">
-        <Wrapper>
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </Wrapper>
+          <MovieCard
+            movies={movies}
+          />
         <Pagination />
       </Container>
     </>
