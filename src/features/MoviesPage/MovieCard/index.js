ad83ae49eaf29f9e2a0882184e2cc05b44dc357e
Karol Galera <frontend.galera@gmail.com>
2022-10-08 14:42:13 +0200

    props added to movieCard component

--- src/features/MoviesPage/MovieCard/index.js
+++ src/features/MoviesPage/MovieCard/index.js
@@ -1,40 +1,52 @@
 import {
-    Card,
-    Content,
-    MovieTitle,
-    NoPosterIcon,
-    Poster, 
-    PosterImage,
-    Rate,
-    Rating,
-    RatingStar,
-    Tag,
-    TagsList,
-    Votes,
-    YearOfProduction
+	Card,
+	Content,
+	MovieTitle,
+	NoPosterIcon,
+	Poster,
+	PosterImage,
+	Rate,
+	Rating,
+	RatingStar,
+	Tag,
+	TagsList,
+	Votes,
+	Wrapper,
+	YearOfProduction
 } from "./styled";
-import poster from "./poster.jpg"
 
-const MovieCard = () => (
-    <Card>
-        <Poster>
-            {poster ? <PosterImage src={poster} /> : <NoPosterIcon />}
-        </Poster>
-        <Content>
-            <MovieTitle>Mulan</MovieTitle>
-            <YearOfProduction>2020</YearOfProduction>
-            <TagsList>
-                <Tag>Action</Tag>
-                <Tag>Adventure</Tag>
-                <Tag>Drama</Tag>
-            </TagsList>
-            <Rating>
-                <RatingStar />
-                <Rate>7,8</Rate>
-                <Votes>35 votes</Votes>
-            </Rating>
-        </Content>
-    </Card>
+const MovieCard = ({ movies }) => (
+	<Wrapper>
+		{movies &&
+			movies.results &&
+			movies.results.map(({ id,
+				poster_path,
+				title,
+				release_date,
+				vote_average,
+				vote_count
+			}) => (
+				<Card key={id}>
+					<Poster>
+						{poster_path ? <PosterImage src={poster_path} /> : <NoPosterIcon />}
+					</Poster>
+					<Content>
+						<MovieTitle>{title}</MovieTitle>
+						<YearOfProduction>{release_date}</YearOfProduction>
+						<TagsList>
+							<Tag>Action</Tag>
+							<Tag>Adventure</Tag>
+							<Tag>Drama</Tag>
+						</TagsList>
+						<Rating>
+							<RatingStar />
+							<Rate>{vote_average}</Rate>
+							<Votes>{vote_count} votes</Votes>
+						</Rating>
+					</Content>
+				</Card>
+			))}
+	</Wrapper>
 );
 
 
