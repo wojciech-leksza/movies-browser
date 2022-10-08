2483a0b6b1a8dda9acd49b2f064de77476592e78
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-08 20:49:22 +0200

    added page and query to slice

--- src/features/MoviesPage/slice.js
+++ src/features/MoviesPage/slice.js
@@ -1,28 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const moviesSlice = createSlice({
     name: 'movies',
     initialState: {
-        movies: null,
         status: "loading",
+        movies: null,
+        query: "",
+        page: 1,
     },
     reducers: {
-        fetchMovies: () => ({
-            movies: null,
-            status: "loading",
-        }),
-        fetchMoviesSuccess: (_, { payload: movies }) => ({
-            movies,
-            status: "success",
-        }),
-        fetchMoviesError: () => ({
-            movies: null,
-            status: "error",
-        }),
+        setQuery: (state, { payload: query }) => {
+            state.query = query;
+        },
+        setPage: (state, { payload: page }) => {
+            state.page = page;
+        },
+        fetchMovies: (state) => {
+            state.movies = null;
+            state.status = "loading";
+        },
+        fetchMoviesSuccess: (state, { payload: movies }) => {
+            state.movies = movies;
+            state.status = "success";
+        },
+        fetchMoviesError: (state) => {
+            state.movies = null;
+            state.status = "error";
+        },
     },
 });
 
 export const {
+    setQuery,
+    setPage,
     fetchMovies,
     fetchMoviesError,
     fetchMoviesSuccess,
@@ -32,5 +42,7 @@ const selectMoviesState = state => state.movies;
 
 export const selectMovies = state => selectMoviesState(state).movies;
 export const selectStatus = state => selectMoviesState(state).status;
+export const selectQuery = state => selectMoviesState(state).query;
+export const selectPage = state => selectMoviesState(state).page;
 
 export default moviesSlice.reducer;
