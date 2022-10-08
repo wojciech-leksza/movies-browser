ff7a5835cb9876f7e96b89c14b43138c355f74a2
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-08 20:54:52 +0200

    added query handling from search bar

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,16 +1,25 @@
+import { useQueryParams } from "../../../features/MoviesPage/queryParams";
 import { Input, StyledIcon, Wrapper, Label } from "./styled";
 
 const SearchBar = () => {
+  const [queryParam, setQueryParam] = useQueryParams();
+
+  const handleQuery = ({ target }) => {
+    setQueryParam(target.value);
+  };
+
   return (
     <Wrapper>
       <Label>
         <StyledIcon />
         <Input
+          value={queryParam || ""}
+          onChange={handleQuery}
           placeholder="Search for movies..."
         />
       </Label>
     </Wrapper>
-  )
+  );
 };
 
 export default SearchBar;
