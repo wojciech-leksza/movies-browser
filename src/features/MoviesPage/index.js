61aef5913509b81852fa0aa03d84ddd92a5ac363
Karol Galera <frontend.galera@gmail.com>
2022-10-11 21:47:54 +0200

    import removed

--- src/features/MoviesPage/index.js
+++ src/features/MoviesPage/index.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { init } from "./slice";
-import { usePageParams } from "./urlParams";
 import Navigation from "../../common/Navigation";
 import Content from "./Content";
 
