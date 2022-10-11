971dfce73234e38698e5732a032f1689e1e7d5f1
Karol Galera <frontend.galera@gmail.com>
2022-10-11 21:42:01 +0200

    tab size changed

--- src/features/MoviesPage/Content/index.js
+++ src/features/MoviesPage/Content/index.js
@@ -10,50 +10,50 @@ import MovieCard from "../MovieCard";
 import Pagination from "../../../common/Pagination";
 
 const Content = () => {
-    const movies = useSelector(selectMovies);
-    const status = useSelector(selectStatus);
-    const query = useSelector(selectQuery);
-    const setPageParams = usePageParams();
+	const movies = useSelector(selectMovies);
+	const status = useSelector(selectStatus);
+	const query = useSelector(selectQuery);
+	const setPageParams = usePageParams();
 
-    switch (status) {
-        case "loading":
-            return (
-                <Container title="Searching for Movies...">
-                    <LoadingPage />
-                </Container>
-            );
-        case "error":
-            return (
-                <Container>
-                    <ErrorPage />
-                </Container>
-            );
-        case "success":
-            if (movies.results.length === 0) {
-                return (
-                    <Container title={`Sorry, there are no results for "${query}"`} >
-                        <NoResultPage />
-                    </Container >
-                );
-            };
+	switch (status) {
+		case "loading":
+			return (
+				<Container title="Searching for Movies...">
+					<LoadingPage />
+				</Container>
+			);
+		case "error":
+			return (
+				<Container>
+					<ErrorPage />
+				</Container>
+			);
+		case "success":
+			if (movies.results.length === 0) {
+				return (
+					<Container title={`Sorry, there are no results for "${query}"`} >
+						<NoResultPage />
+					</Container >
+				);
+			};
 
-            return (
-                <Container title={!!query ? `Search results for "${query}"` : "Popular Movies"}>
-                    <MovieList>
-                        {movies.results.map((movie) => (
-                            <MovieCard
-                                key={movie.id}
-                                movie={movie}
-                            />
-                        ))}
-                    </MovieList>
-                    <Pagination setPageParams={setPageParams}/>
-                </Container>
-            );
+			return (
+				<Container title={!!query ? `Search results for "${query}"` : "Popular Movies"}>
+					<MovieList>
+						{movies.results.map((movie) => (
+							<MovieCard
+								key={movie.id}
+								movie={movie}
+							/>
+						))}
+					</MovieList>
+					<Pagination setPageParams={setPageParams} />
+				</Container>
+			);
 
-        default:
-            break;
-    };
+		default:
+			break;
+	};
 };
 
 export default Content;
