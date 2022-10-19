382c04c7af71b723e3d52a8ce397da4738380e43
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-10-19 16:27:50 +0200

    Fixed elastic container in MovieList and PeopleList

--- src/common/styled.js
+++ src/common/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MovieList = styled.ul`
     display: grid;
     justify-content: center;
-    grid-template-columns: repeat(auto-fit, 324px);
+    grid-template-columns: repeat(auto-fill, 324px);
     gap: 24px;
     margin-top: 24px;
     padding: 0;
@@ -18,7 +18,7 @@ export const MovieList = styled.ul`
 export const PeopleList = styled.article`
     display: grid;
     justify-content: center;
-    grid-template-columns: repeat(auto-fit, 208px);
+    grid-template-columns: repeat(auto-fill, 208px);
     gap: 24px;
     margin-top: 32px;
 
